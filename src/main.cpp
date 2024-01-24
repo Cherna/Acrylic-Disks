f134ea94975e9a9518431fe470d2875cfb204f0e
Tomás Chernov <Chernatom@gmail.com>
2024-01-23 21:26:14 -0300

    get the basics working

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
-#include <Ticker.h>
 
 // Define stepper motor connections and steps per revolution
 #define MOTOR1_STEP_PIN 2
@@ -9,55 +8,48 @@
 #define MOTOR2_DIR_PIN 6
 #define MOTOR3_STEP_PIN 4
 #define MOTOR3_DIR_PIN 7
-
 #define MOTORS_ENABLE 8
+#define MICSTEP 2 // Microstepping set on the driver
 
-AccelStepper stepper1(AccelStepper::FULL4WIRE, MOTOR1_STEP_PIN, MOTOR1_DIR_PIN);
-AccelStepper stepper2(AccelStepper::FULL4WIRE, MOTOR2_STEP_PIN, MOTOR2_DIR_PIN);
-AccelStepper stepper3(AccelStepper::FULL4WIRE, MOTOR3_STEP_PIN, MOTOR3_DIR_PIN);
+#define MOTOR_COUNT 3
 
-AccelStepper steppers[] = {stepper1, stepper2, stepper3};
+AccelStepper stepper1(AccelStepper::DRIVER, MOTOR1_STEP_PIN, MOTOR1_DIR_PIN);
+AccelStepper stepper2(AccelStepper::DRIVER, MOTOR2_STEP_PIN, MOTOR2_DIR_PIN);
+AccelStepper stepper3(AccelStepper::DRIVER, MOTOR3_STEP_PIN, MOTOR3_DIR_PIN);
 
-Ticker timer;
+AccelStepper steppers[MOTOR_COUNT] = {stepper1, stepper2, stepper3};
 
 int direction = 1; // 1 for forward, -1 for backward
 unsigned long previousMillis = 0;
-const long interval = 1000;
+const long interval = 500;
 
 void setup()
 {
   // Set up the serial communication
   Serial.begin(115200);
 
+  pinMode(MOTORS_ENABLE, OUTPUT);
+  digitalWrite(MOTORS_ENABLE, LOW);
+
+
   // Set the maximum speed and acceleration for each stepper motor
   for (auto &motor : steppers)
   {
-    motor.setMaxSpeed(1000.0);
-    motor.setAcceleration(500.0);
+    motor.setMaxSpeed(800 * MICSTEP);
+    motor.setAcceleration(2000 * MICSTEP);
   }
 }
 
+bool anyMotorRunning;
+int counter = 0;
+
 void loop()
 {
+
+  anyMotorRunning = false;
   for (auto &motor : steppers)
   {
-    motor.run();
-  }
-
-  for (auto &motor : steppers)
-  {
-    motor.move(200 * direction);
-  }
-
-  // Check if the motors have reached the target position
-  bool anyMotorRunning;
-  while (anyMotorRunning)
-  {
-    anyMotorRunning = false;
-    for (auto &motor : steppers)
-    {
-      anyMotorRunning = anyMotorRunning || motor.run();
-    }
+    anyMotorRunning = anyMotorRunning || motor.run();
   }
 
   if (!anyMotorRunning)
@@ -65,11 +57,28 @@ void loop()
     unsigned long currentMillis = millis();
     if (currentMillis - previousMillis >= interval)
     {
-      // Reverse the direction
-      direction *= -1;
+      Serial.print("Moving: ");
+      Serial.println(counter);
+      steppers[counter].move(200 * MICSTEP * direction);
+
+      // Increase motor counter
+      ++counter;
+
+      // Reverse the direction after all motors have run
+      if (counter >= MOTOR_COUNT)
+      {
+        Serial.println("Resetting counter and reversing");
+        counter = 0;
+        direction *= -1;
+      }
 
       // Reset the timer for the next interval
       previousMillis = currentMillis;
     }
+  }
+
+  for (auto &motor : steppers)
+  {
+    motor.run();
   }
 }
