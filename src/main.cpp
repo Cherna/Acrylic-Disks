63e5f48b478e83cd8c8d77278b14f3ad38d08517
Tomás Chernov <Chernatom@gmail.com>
2024-04-30 02:42:09 -0300

    Implement missing fns

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,31 @@
 #define MOTOR2_DIR_PIN 6
 #define MOTOR3_STEP_PIN 4
 #define MOTOR3_DIR_PIN 7
+#define MOTOR3_DIR_PIN 9
+#define MOTOR4_STEP_PIN 10
+#define MOTOR4_DIR_PIN 11
+#define MOTOR5_STEP_PIN 12
+#define MOTOR5_DIR_PIN 13
+#define MOTOR6_STEP_PIN 14
+#define MOTOR6_DIR_PIN 15
 #define MOTORS_ENABLE 8
 #define MICSTEP 2 // Microstepping set on the driver
-
-#define MOTOR_COUNT 3
 
 AccelStepper stepper1(AccelStepper::DRIVER, MOTOR1_STEP_PIN, MOTOR1_DIR_PIN);
 AccelStepper stepper2(AccelStepper::DRIVER, MOTOR2_STEP_PIN, MOTOR2_DIR_PIN);
 AccelStepper stepper3(AccelStepper::DRIVER, MOTOR3_STEP_PIN, MOTOR3_DIR_PIN);
+AccelStepper stepper4(AccelStepper::DRIVER, MOTOR4_STEP_PIN, MOTOR4_DIR_PIN);
+AccelStepper stepper5(AccelStepper::DRIVER, MOTOR5_STEP_PIN, MOTOR5_DIR_PIN);
+AccelStepper stepper6(AccelStepper::DRIVER, MOTOR6_STEP_PIN, MOTOR6_DIR_PIN);
 
-AccelStepper steppers[MOTOR_COUNT] = {stepper1, stepper2, stepper3};
+AccelStepper circle1[3] = {stepper1, stepper2, stepper3};
+AccelStepper circle2[3] = {stepper1, stepper2, stepper3};
+AccelStepper allSteppers[6] = {stepper1, stepper2, stepper3, stepper4, stepper5, stepper6};
+
+template <typename T, size_t N>
+size_t getArrayLength(const T (&)[N]) {
+    return N;
+}
 
 void setup() {
   // Set up the serial communication
@@ -29,35 +44,73 @@ void setup() {
 
 
   // Set the maximum speed and acceleration for each stepper motor
-  for (auto &motor : steppers) {
+  for (auto &motor : allSteppers) {
     motor.setMaxSpeed(4000 * MICSTEP);
     motor.setAcceleration(4000 * MICSTEP);
   }
 }
 
 // TODO: Function to run homing sequence
-void homeAllMotors() {}
+void homeAllMotors(AccelStepper (&circle)[3] = circle1) {
+  // Move all motors in a certain direction until the home switches are tripped
+}
+
+int angleToSteps(int angle) {
+  static double hypothenuseInMm = 1700;
+  static double circumferenceInMm = 264;
+  static double distancePerStepInMm = 0.04;
+  double angleInRadians = angle * M_PI / 180.0;
+  double oppositeSideInMm = hypothenuseInMm * sin(angleInRadians);
+
+  return oppositeSideInMm / distancePerStepInMm; 
+}
 
 // TODO: Function to hold specific angle
-void moveCircleToAngle() {} 
+void moveCircleToAngle(
+  AccelStepper (&circle)[3] = circle1,
+  int angle = 10, // Angle in degrees
+  int direction = 0
+) {
+  circle[direction].move(angleToSteps(angle) * MICSTEP);
+}
 
 // TODO: Function to move a circle to a "neutral" horizontal position
-void normalizeMotorsHeight() {}
+void normalizeMotorsHeight(AccelStepper (&circle)[3] = circle1) {
+  // Get the current position of the motor and set the new position to
+  // a round number closest to the average between this circles motors position
+  int avgPos = (
+    circle[0].currentPosition() +
+    circle[1].currentPosition() +
+    circle[2].currentPosition()
+    )/3;
+  
+  for (auto &motor : circle) {
+    motor.moveTo(avgPos);
+  }
+}
 
 // TODO: Function to move entire circle up
-void allMotorsUp() {}
+void allMotorsUp(AccelStepper (&circle)[3] = circle1) {
+  for (auto &motor : circle) {
+    motor.move(-1000 * MICSTEP);
+  }
+}
 
 // TODO: Function to move entire circle down
-void allMotorsDown() {}
+void allMotorsDown(AccelStepper (&circle)[3] = circle1) {
+  for (auto &motor : circle) {
+    motor.move(1000 * MICSTEP);
+  }
+}
 
 void stopAllMotors() {
-  for (auto &motor : steppers) {
+  for (auto &motor : allSteppers) {
     motor.stop();
   }
 }
 
 bool anyMotorMoving() {
-  for (auto &motor : steppers) {
+  for (auto &motor : allSteppers) {
     if (motor.isRunning()) {
       return true;
     }
@@ -65,57 +118,68 @@ bool anyMotorMoving() {
   return false;
 }
 
-
-// TODO: Include this in the runSine function
-int currentMove = 0;
-int nextMove = 1;
 void runSine(
-  int magnitude = 200, // Vertical magnitude of the wave
-  float overlap = 0.5 // Fraction of the wave to overlap with the next wave
+  bool reset = false,
+  bool debug = false,
+  AccelStepper (&circle)[3] = circle1,
+  int magnitude = 200, // Vertical magnitude of the wave in steps
+  float overlap = 0.5 // Fraction of the wave to overlap with the previous move
 ) {
-  int direction = direction || 1;
+  static int currentMove = 0;
+  static int nextMove = 1;
+  if (reset) {
+    currentMove = 0;
+    nextMove = 1;
+  }
+  static int direction = 1;
 
-  for(auto &motor : steppers) {
+  for(auto &motor : circle) {
     motor.setMaxSpeed(100 * MICSTEP);
   }
 
+  // Start by moving the first motor if no motor is moving
   if (!anyMotorMoving()) {
-    steppers[currentMove].move(magnitude * MICSTEP * direction);
+    circle[currentMove].move(magnitude * MICSTEP * direction);
   }
 
-  // Serial.println(abs(steppers[currentMove].distanceToGo()));
-  // Serial.println(magnitude * MICSTEP * overlap);
+  if (debug) {
+    Serial.println(abs(circle[currentMove].distanceToGo()));
+    Serial.println(magnitude * MICSTEP * overlap);
 
-  // Serial.print("Current move: ");
-  // Serial.println(currentMove);
-  // Serial.print("Next move: ");
-  // Serial.println(nextMove);
+    Serial.print("Current move: ");
+    Serial.println(currentMove);
+    Serial.print("Next move: ");
+    Serial.println(nextMove);
+  };
 
   if (
-    steppers[currentMove].isRunning() &&
-    abs(steppers[currentMove].distanceToGo()) <= magnitude * MICSTEP * overlap
+    circle[currentMove].isRunning() &&
+    abs(circle[currentMove].distanceToGo()) <= magnitude * MICSTEP * overlap
   ) {
-    Serial.println("Starting next move");
-    steppers[nextMove].move(magnitude * MICSTEP * direction);
+    if (debug) {
+      Serial.println("Starting next move");
+    };
+    circle[nextMove].move(magnitude * MICSTEP * direction);
+
     // Reverse current move direction
-    if (currentMove == MOTOR_COUNT - 1) {
-      direction *= -1;
-    }
-    // Advance currentMove or reset to 0 if we've reached the end
-    if (currentMove == (MOTOR_COUNT - 1)) {
-      currentMove = 0;
-    } else if (nextMove == (MOTOR_COUNT - 1)) {
-      nextMove = 0;
-    } else {
-      currentMove++;
-      nextMove++;
-    }
+    direction *= (currentMove == getArrayLength(circle) - 1) ? -1 : 1;
+    currentMove = (currentMove == getArrayLength(circle) - 1) ? 0 : currentMove + 1;
+    nextMove = (nextMove == getArrayLength(circle) - 1) ? 0 : nextMove + 1;
+    // // Advance currentMove or reset to 0 if we've reached the end
+    // if (currentMove == (MOTOR_COUNT - 1)) {
+    //   currentMove = 0;
+    // } else if (nextMove == (MOTOR_COUNT - 1)) {
+    //   nextMove = 0;
+    // } else {
+    //   currentMove++;
+    //   nextMove++;
+    // }
   }
 }
 
-void runSequentially() {
+void runSequentially(AccelStepper (&circle)[3] = circle1) {
   int counter = 0;
-  counter = counter == MOTOR_COUNT - 1 ? 0 : counter;
+  counter = counter == getArrayLength(circle) - 1 ? 0 : counter;
   unsigned long previousMillis = previousMillis || 0;
   int direction = direction || 1;
   if (anyMotorMoving()) {
@@ -126,13 +190,13 @@ void runSequentially() {
     if (currentMillis - previousMillis >= 500) {
       Serial.print("Moving: ");
       Serial.println(counter);
-      steppers[counter].move(200 * MICSTEP * direction);
+      allSteppers[counter].move(200 * MICSTEP * direction);
 
       // Increase motor counter
       ++counter;
 
       // Reverse the direction after all motors have run
-      if (counter >= MOTOR_COUNT) {
+      if (counter >= getArrayLength(circle)) {
         Serial.println("Resetting counter and reversing");
         counter = 0;
         direction *= -1;
@@ -148,7 +212,7 @@ void loop() {
   // runSequentially();
   runSine();
 
-  for (auto &motor : steppers) {
+  for (auto &motor : allSteppers) {
     motor.run();
   }
 }
